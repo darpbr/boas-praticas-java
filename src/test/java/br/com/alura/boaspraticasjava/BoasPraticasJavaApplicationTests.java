943d9d006decda7aa8a69795e647e93b2efcd48d
darpbr <dipeixoto@gmail.com>
2023-09-22 18:56:41 -0300

    first commit

---
+++ src/test/java/br/com/alura/boaspraticasjava/BoasPraticasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.boaspraticasjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoasPraticasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
